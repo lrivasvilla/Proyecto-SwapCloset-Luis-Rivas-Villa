868c58598d0188ec0de3eed301664ac6dd2492ab
Luis Rivas Villa <lrivasvilla@safareyes.es>
2026-01-26 22:34:57 +0100

    Cambiando main

--- Swap-Closet-V5-despliegue/frontend/src/main.ts
+++ Swap-Closet-V5-despliegue/frontend/src/main.ts
@@ -9,7 +9,7 @@ import {provideHttpClient} from "@angular/common/http";
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    provideIonicAngular({ animated: false }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient()
   ],
